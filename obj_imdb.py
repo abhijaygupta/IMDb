ef55a30ac34b8a3d6c53a91183216103f5ae90e2
Abhijay <abhijaygupta@hotmail.com>
2016-04-23 22:02:28 -0400

    Stage 1 over

---
+++ obj_imdb.py
@@ -0,0 +1,82 @@
+import requests
+import urllib2
+from bs4 import BeautifulSoup
+
+#name1 = raw_input('Enter the first actor/actress name: ')
+#name2 = raw_input('Enter the second actor/actress name: ')
+
+name1 = "Brad Pitt"
+name2 = "George Clooney" 
+#name1 = name2.lower()
+#nam2 = name2.lower()
+
+#artist = '\"' + artist + ' '
+#song = song + '\"'
+
+imdbWebsite = 'imdb+'
+url = 'http://google.com/search?sourceid=navclient&btnI=1&q=' #imdbWebsite+%s
+url = url+imdbWebsite+'\"'+name1+'\"'
+print url
+
+url2 = 'http://google.com/search?sourceid=navclient&btnI=1&q=' #imdbWebsite+%s
+url2 = url2+imdbWebsite+'\"'+name2+'\"'
+print url2
+
+
+var = requests.get(url)
+print var.url
+
+html = urllib2.urlopen(var.url).read()
+soup = BeautifulSoup(html, "html.parser")
+
+content = soup.find("div", {"class": "filmo-category-section"})
+
+#print content
+paras = content.findAll('b')
+
+#print paras
+
+list1 = []
+for para in paras:
+	#print '\n'
+	list1.append(para.getText())
+	#print para.getText()
+
+
+var2 = requests.get(url2)
+print var2.url
+
+html2 = urllib2.urlopen(var2.url).read()
+soup2 = BeautifulSoup(html2, "html.parser")
+
+content2 = soup2.find("div", {"class": "filmo-category-section"})
+
+#print content2
+paras2 = content2.findAll('b')
+
+#print paras2
+
+list2 = []
+for para2 in paras2:
+	#print '\n'
+	list2.append(para2.getText())
+	#print para.getText()
+print '\n'
+
+print list1
+print(' ')
+print list2
+print(' ' )
+print set(list1).intersection(list2)
+
+
+
+
+
+
+
+
+
+
+
+
